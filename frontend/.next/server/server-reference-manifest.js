0ddde31198940a9a4d96d39ad86b7198e7a8d285
kolbeyang <kky2806@rit.edu>
2025-02-22 14:52:36 -0500

    feat: login button gitignore changes

--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"EtK7HGIK+cdbkK7HWsIo/rgaE8pTC/nJ6pTR1QAJROA=\"\n}"
--- frontend/src/components/Login/index.tsx
+++ frontend/src/components/Login/index.tsx
@@ -1,7 +1,18 @@
 "use client";
 
+import { useAuth0 } from "@auth0/auth0-react";
+import Button from "../ui/Button";
+
 const Login = () => {
-  return <div>Login</div>;
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <div className="size-full flex-1">
+      <Button size="lg" className="w-full" onClick={() => loginWithRedirect()}>
+        Login
+      </Button>
+    </div>
+  );
 };
 
 export default Login;
