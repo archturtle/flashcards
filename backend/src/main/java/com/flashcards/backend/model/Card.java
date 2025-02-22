6f4fa4d8ea5084f207ca6460b4c87b2f5680557a
Aayan Sayed <98489656+aas1128@users.noreply.github.com>
2025-02-22 12:48:48 -0500

    Test

--- backend/src/main/java/com/flashcards/backend/model/Card.java
+++ backend/src/main/java/com/flashcards/backend/model/Card.java
@@ -1,8 +1,24 @@
-import org.springframework.data.annotation.Id;;
+package com.flashcards.backend.model;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+@Data
+
 public class Card {
     @Id
-    public String id;
+    public ObjectId id;
+    public String front;
+    public String back;
     public Card (String front, String back) {
+            this.front = front; 
+            this.back = back;  
+    } 
+
+    public void toString (Card card) {
+        System.out.print("ID of the Card" + this.id);
+        System.out.println("Front of the Card: " + this.front);
+        System.out.println("Back of the Card: " + this.back);
 
     }
 
