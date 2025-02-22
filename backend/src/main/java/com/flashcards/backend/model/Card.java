fdea59ac3277d180249ae63c2e784c50de9ea358
Siddhartha Juluru <siddhartha.juluru@gmail.com>
2025-02-22 14:47:00 -0500

    mongo & openai stuff

--- backend/src/main/java/com/flashcards/backend/model/Card.java
+++ backend/src/main/java/com/flashcards/backend/model/Card.java
@@ -2,17 +2,20 @@ package com.flashcards.backend.model;
 
 import org.springframework.data.annotation.Id;
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document("card")
 public class Card {
     @Id
     public String id;
     public String front;
     public String back;
 
-    public Card (String front, String back) {
-            this.front = front; 
-            this.back = back;
+    public Card (String id, String front, String back) {
+        this.id = id;
+        this.front = front;
+        this.back = back;
     }
 
     public String toString() {
--- backend/src/main/java/com/flashcards/backend/persistence/UserDAO.java
+++ backend/src/main/java/com/flashcards/backend/persistence/UserDAO.java
@@ -1,12 +1,10 @@
 package com.flashcards.backend.persistence;
 
 import com.flashcards.backend.model.User;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import com.flashcards.backend.model.Card;
 
-import java.util.ArrayList;
-
-public interface UserDAO extends MongoRepository<Card, String> {
-    public User createUser (User user);
-    public User findUserByEmail (String email);
+public interface UserDAO extends MongoRepository<User, String> {
+    public ObjectId createUser(User user);
+    public User findUserByEmail(String email);
 }
