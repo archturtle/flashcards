43561302caf7f1286febea65d35955d1c261725b
Finn <fss5045@rit.edu>
2025-02-22 20:27:49 -0500

    adding auth header w token to fetch requests w axios instance

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -5,22 +5,22 @@ const axinst = axios.create({
     baseURL: "http://localhost:8080"
 });
 
-axinst.interceptors.request.use(
-    async (config) => {
-        try {
-            const {getAccessTokenSilently} = useAuth0();
-            const token = await getAccessTokenSilently()
-            if (token) {
-                config.headers.Authorization = `Bearer ${token}`;
+export const setAuthTokenInterceptor = (getAccessTokenSilently: () => Promise<string>) => {
+    axinst.interceptors.request.use(
+        async (config) => {
+            try {
+                const token = await getAccessTokenSilently()
+                if (token) {
+                    console.log(token)
+                    config.headers.Authorization = `Bearer ${token}`;
+                }
+            } catch (error) {
+                console.error("Error getting Auth0 token:", error)
             }
-        } catch (error) {
-            console.error("Error getting Auth0 token:", error)
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error)
-    }
-);
+            return config;
+        },
+        (error) => Promise.reject(error)
+    );
+};
 
 export default axinst;
